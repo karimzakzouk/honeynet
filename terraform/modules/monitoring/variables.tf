82532994ae1ff97d9af0e29447dc8ef72d71d39d
Karim Farid <karimzakzouk69@gmail.com>
2026-03-29 07:54:20 +0200

    remove provider block from monitoring and honeypot modules

--- terraform/modules/monitoring/variables.tf
+++ terraform/modules/monitoring/variables.tf
@@ -2,7 +2,3 @@ variable "honeypot_ips" {
   description = "IPs of honeypot nodes for Prometheus to scrape"
   type        = list(string)
 }
-EOF\cat > terraform/modules/monitoring/outputs.tf << 'EOF'
-output "grafana_url" {
-  value = "http://${aws_instance.monitoring.public_ip}:3000"
-}
